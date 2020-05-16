c5854f1cba1f1a678bcf0d6da4ddce37b2f8d60e
Simon Zeng <simonzeng9@gmail.com>
2020-05-16 16:33:45 -0700

    added reversibility between main page and secondary pages like cooking and coding

--- src/pages/coding.jsx
+++ src/pages/coding.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import SearchBarButton from "../assets/components/SearchBarButton.jsx";
 
 const CodingPage = () => {
   return (
-    <div>
-      <h3> Coding</h3>
+    <div className="side-projects">
+      <Navbar className="border-nav" variant="light" sticky="top">
+        <Container>
+          <Nav.Link href="./">Main</Nav.Link>
+          <Nav.Link href="#skills">Skills</Nav.Link>
+          <Nav.Link href="#projects">Projects</Nav.Link>
+          <Nav.Link href="#contact">Contact</Nav.Link>
+          <SearchBarButton></SearchBarButton>
+        </Container>
+      </Navbar>
     </div>
   );
 };
--- src/pages/cooking.jsx
+++ src/pages/cooking.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import SearchBarButton from "../assets/components/SearchBarButton.jsx";
 
 const CookingPage = () => {
   return (
-    <div>
-      <h3> Cooking</h3>
+    <div className="cooking">
+      <Navbar className="border-nav" variant="light" sticky="top">
+        <Container>
+          <Nav.Link href="./">Main</Nav.Link>
+          <Nav.Link href="#skills">Skills</Nav.Link>
+          <Nav.Link href="#projects">Projects</Nav.Link>
+          <Nav.Link href="#contact">Contact</Nav.Link>
+          <SearchBarButton></SearchBarButton>
+        </Container>
+      </Navbar>
     </div>
   );
 };
