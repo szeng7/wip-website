179c3ceae8fc8a972ec349e14838f6be80619e9c
Simon Zeng <simonzeng9@gmail.com>
2020-05-18 06:25:26 -0700

    Updates

---
+++ precache-manifest.abdc8ea07de6c5b4345f25e93280b4c9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a66a4dec22450e28472edc0166ba4d6e",
+    "url": "/wip-website/index.html"
+  },
+  {
+    "revision": "f6a4605f103d70954ca6",
+    "url": "/wip-website/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "7286482138f452ad3eb7",
+    "url": "/wip-website/static/css/main.f3d7b384.chunk.css"
+  },
+  {
+    "revision": "f6a4605f103d70954ca6",
+    "url": "/wip-website/static/js/2.a6a61e67.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/wip-website/static/js/2.a6a61e67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7286482138f452ad3eb7",
+    "url": "/wip-website/static/js/main.6ab17777.chunk.js"
+  },
+  {
+    "revision": "981082e31aff3a053515",
+    "url": "/wip-website/static/js/runtime-main.95743bf2.js"
+  },
+  {
+    "revision": "b90979fa2a5d7d23f89628694c89eeec",
+    "url": "/wip-website/static/media/SimonZengResumeSofDev.b90979fa.pdf"
+  },
+  {
+    "revision": "003c9374b399cc366da663479969a127",
+    "url": "/wip-website/static/media/cake1.003c9374.jpg"
+  },
+  {
+    "revision": "aa46b65842cfa33b9f46837c2606eade",
+    "url": "/wip-website/static/media/jhu.aa46b658.png"
+  },
+  {
+    "revision": "80a2b3fcdb6ce28d99369ce7a348ff66",
+    "url": "/wip-website/static/media/profpic.80a2b3fc.jpg"
+  }
+]);
