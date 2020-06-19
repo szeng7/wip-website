fe78c94121f38b9c87b5678cad5f36c00d7e9003
Simon Zeng <simonzeng9@gmail.com>
2020-06-19 07:19:19 -0700

    added mobile timeline

--- src/assets/components/Timeline.jsx
+++ src/assets/components/Timeline.jsx
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
-import { Col, Row, Tab, Nav, Container } from "react-bootstrap";
+import { Col, Row, Tab, Nav, Container, Tabs } from "react-bootstrap";
 
 class Timeline extends Component {
     constructor(props) {
         super(props);
         this.state = {
             display: 0,
+            isDesktop: false,
         };
+        this.updatePredicate = this.updatePredicate.bind(this);
+    }
+    componentDidMount() {
+        this.updatePredicate();
+        window.addEventListener("resize", this.updatePredicate);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.updatePredicate);
+    }
+    updatePredicate() {
+        this.setState({ isDesktop: window.innerWidth > 600 });
     }
 
     createTabs() {
@@ -34,6 +47,51 @@ class Timeline extends Component {
                     )
                 )}
             </Nav>
+        );
+    }
+
+    createMobileTabs() {
+        return this.props.timelineItems.map(
+            (
+                {
+                    tab,
+                    company,
+                    title,
+                    location,
+                    description,
+                    image,
+                    firstLinkPath,
+                    firstLinkText,
+                    secondLinkPath,
+                    secondLinkText,
+                },
+                index
+            ) => (
+                <Tab eventKey={index} title={tab}>
+                    <Container>
+                        <Row>
+                            <h1>{company}</h1>
+                        </Row>
+                        <Row>
+                            <h2>{title}</h2>
+                        </Row>
+                        <Row>
+                            <h3>{location}</h3>
+                        </Row>
+                        <Row>
+                            <p>{description}</p>
+                        </Row>
+                        <Row>
+                            {this.addLinks(
+                                firstLinkPath,
+                                firstLinkText,
+                                secondLinkPath,
+                                secondLinkText
+                            )}
+                        </Row>
+                    </Container>
+                </Tab>
+            )
         );
     }
 
@@ -113,20 +171,28 @@ class Timeline extends Component {
     }
 
     render() {
-        return (
-            <Container fluid className="timeline-container">
-                <Tab.Container defaultActiveKey="1" className="list-inline">
-                    <Row>
-                        <Col xs={12} md={{ span: 3, offset: 1 }}>
-                            {this.createTabs()}
-                        </Col>
-                        <Col xs={12} md={7}>
-                            {this.createPanes()}
-                        </Col>
-                    </Row>
-                </Tab.Container>
-            </Container>
-        );
+        if (this.state.isDesktop) {
+            return (
+                <Container fluid className="timeline-container">
+                    <Tab.Container defaultActiveKey="1" className="list-inline">
+                        <Row>
+                            <Col xs={12} md={{ span: 3, offset: 1 }}>
+                                {this.createTabs()}
+                            </Col>
+                            <Col xs={12} md={7}>
+                                {this.createPanes()}
+                            </Col>
+                        </Row>
+                    </Tab.Container>
+                </Container>
+            );
+        } else {
+            return (
+                <Container fluid className="timeline-container-mobile">
+                    <Tabs defaultActiveKey="1">{this.createMobileTabs()}</Tabs>
+                </Container>
+            );
+        }
     }
 }
 export default Timeline;
